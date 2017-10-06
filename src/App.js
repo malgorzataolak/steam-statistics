2736979b33caf07a022e335ba37f1e59c163e7f1
Malgorzata Olak <m.olak@outlook.com>
2017-10-06 20:28:00 +0200

    Components with default content added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,92 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Menu />
+          <Header />
+          <Content />
       </div>
     );
   }
+}
+
+class Menu extends Component{
+    render(){
+      return(
+        <div className="Menu">
+            <nav>
+                <ul> 
+                  <li>Top 100 Games</li>
+                  <li>Statistics</li>
+                </ul>
+            </nav>
+        </div>
+        );
+    }
+
+}
+
+class Header extends Component{
+    render(){
+        return(
+          <div className="Header">
+            <header>
+                <h1>Default header</h1>
+            </header>
+          </div>
+        );
+    }
+
+}
+
+class Content extends Component{
+      render(){
+          return(
+            <div className="Content">
+              <MainView />
+              <TableView />
+              <ChartView />
+            </div>
+          );
+      }
+
+}
+
+class MainView extends Component{
+      render(){
+        return(
+          <div className="MainView">
+              <p>MainView content</p>
+          </div>
+        );
+      }
+
+}
+
+class TableView extends Component{
+      render(){
+        return(
+          <div className="TableView">
+              <p>TableView content</p>
+          </div>
+        );
+      }
+}
+
+class ChartView extends Component{
+      render(){
+        return(
+          <div className="ChartView">
+              <p>ChartView content</p>
+          </div>
+        );
+      }
+
 }
 
 export default App;
